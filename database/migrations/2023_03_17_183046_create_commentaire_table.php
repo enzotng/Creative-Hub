20b8210a7e29633be00e0ff9129e0bf85842a22c
Morgan Kpassi <morgan.kpassi@gmail.com>
2023-03-17 19:36:07 +0100

    ajout table commentaire

---
+++ database/migrations/2023_03_17_183046_create_commentaire_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commentaire_table', function (Blueprint $table) {
+            $table->increments('id_commentaire');
+            $tabel->text('contenu_commentaire');
+            $table->datetime('date_commentaire');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commentaire_table');
+    }
+};
